3de7e3d6d8882c5aec109cfb693797be53dd0223
Matteo Tatoni <83955605+TatoniMatteo@users.noreply.github.com>
2023-12-16 22:41:06 +0100

    Update Repository

--- src/main/java/com/tatonimatteo/campfinder/repository/UserRepository.java
+++ src/main/java/com/tatonimatteo/campfinder/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.tatonimatteo.campfinder.repository;
 
 import com.tatonimatteo.campfinder.entity.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findById(long id);
+
 }
--- src/main/java/com/tatonimatteo/campfinder/service/UserServiceImpl.java
+++ src/main/java/com/tatonimatteo/campfinder/service/UserServiceImpl.java
@@ -2,14 +2,14 @@ package com.tatonimatteo.campfinder.service;
 
 import com.tatonimatteo.campfinder.entity.User;
 import com.tatonimatteo.campfinder.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-    private final UserRepository repository;
+    @Autowired
+    private UserRepository repository;
 
     @Override
     public User saveUser(User user) {
