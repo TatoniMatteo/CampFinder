32ed967991333e981e50f8d19ba120201b203259
Matteo Tatoni <83955605+TatoniMatteo@users.noreply.github.com>
2024-01-12 00:28:02 +0100

    Paginazione
    Creazione paginazione
    Creazione pagina errore 500

--- src/main/java/com/tatonimatteo/campfinder/controller/BaseController.java
+++ src/main/java/com/tatonimatteo/campfinder/controller/BaseController.java
@@ -23,11 +23,14 @@ public class BaseController {
     public String search(@RequestParam(required = false, defaultValue = "") String query,
                          @RequestParam(required = false, defaultValue = "false") boolean tent,
                          @RequestParam(required = false, defaultValue = "false") boolean bed,
+                         @RequestParam(required = false, defaultValue = "1") int page,
                          Model model) {
         model.addAttribute("query", query);
         model.addAttribute("tent", tent);
         model.addAttribute("bed", bed);
+        model.addAttribute("page", page);
         model.addAttribute("places", placeService.searchPlace(query, tent, bed, 20, 0));
+        model.addAttribute("pages", 5);
         return "search";
     }
 
